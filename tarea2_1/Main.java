78f65b8b422dd17aeae24809c2e44ab912125f69
Jaime <56158062+jaimgarod10@users.noreply.github.com>
2020-04-04 12:25:09 +0200

    tarea2

---
+++ tarea2_1/Main.java
@@ -0,0 +1,19 @@
+package com.mycompany.tarea2_1;
+
+/**
+ *
+ * @author jaimg
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       tarea2_1 t = new tarea2_1();
+        
+    }
+    
+}
+
---
+++ tarea2_2/tarea2_2.java
@@ -0,0 +1,19 @@
+package com.mycompany.tarea2_2;
+
+/**
+ *
+ * @author jaimg
+ */
+public class tarea2_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        tarea2_2 t = new tarea2_2();
+        
+    }
+    
+}
+
